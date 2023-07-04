bf1697525a1649e743cb4314bf34b7873af46c88
Saeed Ganji <shahabganji@gmail.com>
2023-07-04 14:03:26 +0200

    wip: Learning stuff from different sources ;)

--- Dddfier.Console/Program.cs
+++ Dddfier.Console/Program.cs
@@ -4,6 +4,47 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Formatting;
 using Dddfier.Console;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.Text;
+
+const string path = @"C:\Users\saeed.ganji\.dev\ringana\RightsApi\Ringana.Rights.sln";
+
+MSBuildLocator.RegisterDefaults();
+
+using var workspace = MSBuildWorkspace.Create();
+var solution = await workspace.OpenSolutionAsync(path);
+
+foreach (var project in solution.Projects)
+{
+    Console.WriteLine(project.AssemblyName);
+}
+
+var document = solution.Projects.SelectMany(p => p.Documents.Where(m => m.Name == "IApiMarker.cs")).Single();
+
+var sourceText = SourceText.From(
+    """
+    using System.Diagnostics.CodeAnalysis;
+
+    namespace Ringana.Rights.Api;
+
+    [SuppressMessage("Design", "CA1040:Avoid empty interfaces")]
+    public interface IApiMarker
+    {
+    }
+
+    public interface IComeFromSourceGenerators
+    {
+    }
+    """);
+
+// everything related to this newDocument is new instances of their type, e.g. new project, new solution and so on.
+var newDocument = document.WithText(sourceText);
+
+var changedApplied = workspace.TryApplyChanges(newDocument.Project.Solution);
+
+Console.WriteLine(changedApplied ? "Applied" : "Something went wrong");
+
 
 var tree = CSharpSyntaxTree.ParseText(
     // lang=csharp
@@ -21,11 +62,16 @@ var tree = CSharpSyntaxTree.ParseText(
     }
     """);
 
-var root = tree.GetRoot();
-var rewriter = new MyIfRewriter();
-var newRoot = rewriter.Visit(root);
 
-Console.WriteLine(newRoot.ToFullString());
+#region syntax rewriter : 
+
+// var root = tree.GetRoot();
+// var rewriter = new MyIfRewriter();
+// var newRoot = rewriter.Visit(root);
+//
+// Console.WriteLine(newRoot.ToFullString());
+
+#endregion
 
 #region syntax Walker : 
 
