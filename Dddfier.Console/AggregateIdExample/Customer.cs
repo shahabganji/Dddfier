5757c0ca21980fe81164b33f8ecc2874f4dd6586
Saeed Ganji <shahabganji@gmail.com>
2023-07-07 13:59:14 +0200

    wip: starting the aggregate root stuff

---
+++ Dddfier.Console/AggregateIdExample/Customer.cs
@@ -0,0 +1,9 @@
+﻿using Dddfier.GeneratedCode;
+using Dddfier.GeneratedCode.Attributes;
+
+namespace Dddfier.Console.AggregateIdExample;
+
+[WithIdOf<Guid>(PropertyName = "CustomerId", SetterModifier = SetterModifier.Protected)]
+public partial class Customer
+{
+}
