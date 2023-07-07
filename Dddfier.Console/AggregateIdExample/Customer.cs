15c83ce89543cdbfb384ddcfe01317120e516a01
Saeed Ganji <shahabganji@gmail.com>
2023-07-07 23:57:36 +0200

    wip: removing the global from the usage for the keywordTypes

--- Dddfier.Console/AggregateIdExample/Customer.cs
+++ Dddfier.Console/AggregateIdExample/Customer.cs
@@ -6,13 +6,5 @@ namespace Dddfier.Console.AggregateIdExample;
 [WithIdOf<Guid>(PropertyName = "CustomerId", SetterModifier = SetterModifier.Protected)]
 public partial class Customer
 {
-
-    public Customer(Guid id)
-    {
-        this.CustomerId = (CustomerId)id;
-    }
-
-    public Guid ToGuid() => CustomerId;
-    
     public override string ToString() => this.CustomerId.ToString();
 }
