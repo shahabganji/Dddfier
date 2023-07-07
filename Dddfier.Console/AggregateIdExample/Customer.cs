9ed1fdf46788e2a5fe597b747b60ca14dd7a1a68
Saeed Ganji <shahabganji@gmail.com>
2023-07-07 23:46:51 +0200

    wip: basics of the Id classes

--- Dddfier.Console/AggregateIdExample/Customer.cs
+++ Dddfier.Console/AggregateIdExample/Customer.cs
@@ -6,4 +6,13 @@ namespace Dddfier.Console.AggregateIdExample;
 [WithIdOf<Guid>(PropertyName = "CustomerId", SetterModifier = SetterModifier.Protected)]
 public partial class Customer
 {
+
+    public Customer(Guid id)
+    {
+        this.CustomerId = (CustomerId)id;
+    }
+
+    public Guid ToGuid() => CustomerId;
+    
+    public override string ToString() => this.CustomerId.ToString();
 }
---
+++ Dddifier/Models/WithIdOfAttributeContextModel.cs
@@ -0,0 +1,4 @@
+namespace Dddifier.Models;
+
+internal sealed record WithIdOfAttributeContextModel(string Namespace, string ClassName, string PropertyName,
+    string TypeName);
