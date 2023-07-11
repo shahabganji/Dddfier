74026a72bbd48097bfdf7ba5bd243c5ef1f34d86
Saeed Ganji <shahabganji@gmail.com>
2023-07-11 07:44:46 +0200

    wip: getting the green build without the VSIX project

--- src/Dddfier/Models/WithIdOfAttributeContextModel.cs
+++ src/Dddfier/Models/WithIdOfAttributeContextModel.cs
@@ -1,4 +1,10 @@
 namespace Dddfier.Models;
 
-internal sealed record WithIdOfAttributeContextModel(string Namespace, string ClassName, string PropertyName,
-    string TypeName);
+internal sealed record WithIdOfAttributeContextModel(
+    string Namespace, string ClassName, string PropertyName, string TypeName)
+{
+    public string Namespace { get; } = Namespace;
+    public string ClassName { get; } = ClassName;
+    public string PropertyName { get; } = PropertyName;
+    public string TypeName { get; } = TypeName;
+}
